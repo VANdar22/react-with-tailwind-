96ffe0d51dc76628e1041f287da60d1535825401
Armahkyek22 <armahkyeku22@gmail.com>
2025-11-27 10:17:38 +0000

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,34 @@ export default {
         "2xl": "1400px",
       },
     },
+    colors: {
+      // Include default colors
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        // ... other gray shades
+      },
+      red: {
+        600: '#dc2626', // This is the default red-600
+        // ... other red shades
+      },
+      // ... other default colors
+    },
+    fontSize: {
+      'xs': '0.75rem',
+      'sm': '0.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '3.75rem',
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
